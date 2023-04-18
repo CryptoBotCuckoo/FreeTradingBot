8a6b36d3d9d3237b8f890f61a6b38bee746b17af
FreeBot <121530654+CryptoBotCuckoo@users.noreply.github.com>
2023-04-19 07:19:47 +0800

    Add files via upload

---
+++ okxHFT_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /home/python1/OkxHFTTradingBot
+mv okxHFTbot.zip /home/python1/OkxHFTTradingBot
+cd /home/python1/OkxHFTTradingBot
+unzip okxHFTbot.zip
+cp -rf okxHFTbot/*.* ./
+cp -rf okxHFTbot/config.ini ./
+rm -rf okxHFTbot.zip okxHFTbot
+tar zxvf ccxt.tar.gz
+rm -rf ccxt.tar.gz
+cp -rf printMoney4.service /lib/systemd/system/
+systemctl daemon-reload
+systemctl enable printMoney4.service
+rm -rf printMoney4.service
+
